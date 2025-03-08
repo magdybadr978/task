3127dfd26f68b4e80a4e94905996b5dcb33b0654
magdybadr978 <magdibadr2002@gmail.com>
2025-03-08 17:00:11 +0200

    delete firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,10 @@
-// const admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
 
-// if (!admin.apps.length) {
-//     const serviceAccount = require("../../a2y1-a9142-firebase-adminsdk-fbsvc-815401f1cb.json");
+if (!admin.apps.length) {
+    const serviceAccount = require("");
   
-//     admin.initializeApp({
-//       credential: admin.credential.cert(serviceAccount),
-//     });
-//   }
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  }
